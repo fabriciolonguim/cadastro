a6bd6c55234a973d8e7693883b548df4c4d5b8a9
fabriciolonguim <fblong.longuim657@gmail.com>
2018-08-27 19:08:16 -0300

    curso terminado

--- banco_de_dados/delete.php
+++ banco_de_dados/delete.php
@@ -5,7 +5,7 @@
 
 
  $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_SPECIAL_CHARS);
- $queryDelete = $link->query("delete from tb_clientes where id='$id");
+ $queryDelete = $link->query("delete from tb_clientes where id='$id'");
  $affected_rows = mysqli_affected_rows($link);
  if($affected_rows > 0):
     header("Location:../consultas.php");
